11af952521d3f7a3bddb1a3c2d0a7a9552f69ac9
aurelien.tamasle@gmail.com <aurelien.tamasle@gmail.com@6b3faf41-f281-541d-591f-9982f2b7a5ee>
2014-11-28 16:35:48 +0000

    Mise à jour des fichiers du chatsystem

---
+++ chatsystem/chatsystemTDa2/Goodbye.java
@@ -0,0 +1,16 @@
+package chatsystemTDa2;
+
+public class Goodbye extends Message
+{
+   private static final long serialVersionUID = 3L;
+
+   private String nickname;
+
+   public Goodbye(String nickname){
+          this.nickname = nickname;
+   }
+
+   public String getNickname(){
+          return this.nickname;
+   }
+}
---
+++ chatsystem/chatsystemTDa2/Send.java
@@ -0,0 +1,20 @@
+package chatsystemTDa2;
+
+/** One text message, with UID for ack system. */
+public class Send extends Message
+{
+  
+	private static final long serialVersionUID = 1L;
+
+	private String message;
+	private int id;
+
+	public Send(String msg, int id)
+	{
+      message = msg;
+      this.id = id;
+	}
+
+	public String getMessage() {return message;}
+	public int getID()         {return id;}
+}
