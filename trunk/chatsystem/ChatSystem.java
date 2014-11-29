903473b6f10b9b34b1138370ffb930fd225c5210
aurelien.tamasle@gmail.com <aurelien.tamasle@gmail.com@6b3faf41-f281-541d-591f-9982f2b7a5ee>
2014-11-29 13:18:04 +0000

    Dernier commit avant de passer sur les test réseaux sous windows avec machines virtuelles

---
+++ trunk/chatsystem/ChatSystem.java
@@ -0,0 +1,44 @@
+package chatsystem;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
+
+import chatsystem.network.ChatNI;
+import chatsystem.network.UDPReceiver;
+import chatsystem.network.UDPSender;
+
+public class ChatSystem {
+	
+	private ChatNI ni;
+	private ChatController controller;
+	
+	public static int NUMBER=0;
+	
+	public ChatSystem(ChatNI ni,ChatController controller){
+		this.ni=ni;
+		this.controller=controller;
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Chat System v1.0");
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Votre nom : ");
+		String nom=sc.nextLine();
+		byte[] buffer = new byte[ChatNI.MAX_SIZE_BUFFER];
+		DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+		try{
+			ChatNI ni= new ChatNI(new UDPReceiver(packet), new UDPSender(new DatagramSocket()));
+			ChatController controller=new ChatController(ni, new User(nom, InetAddress.getLocalHost()));
+			
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+		
+		
+
+	}
+
+}
